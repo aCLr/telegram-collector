9fdff206cf842d42b3795badaa635e93202ccf51
aCLr <a.ch.clr@gmail.com>
2020-11-01 10:39:42 +0300

    async

--- examples/main.rs
+++ examples/main.rs
@@ -1,11 +1,12 @@
 use log;
 use simple_logger;
-use std::sync::{mpsc, Arc, Mutex, RwLock};
+use std::sync::Arc;
 
 use chrono::{NaiveDate, NaiveDateTime};
 use futures::stream::StreamExt;
 use tg_collector::config::Config;
 use tg_collector::tg_client::{TgClient, TgUpdate};
+use tokio::sync::{mpsc, Mutex, RwLock};
 use tokio::task::spawn;
 
 #[tokio::main]
@@ -20,11 +21,11 @@ async fn main() {
         phone_number: env!("TG_PHONE").to_string(),
     };
     let main_api = Arc::new(RwLock::new(TgClient::new(&conf)));
-    let (sender, receiver) = mpsc::channel::<TgUpdate>();
+    let (sender, receiver) = mpsc::channel::<TgUpdate>(2000);
     let mut join_handle = None;
     let chats = {
         println!("lock");
-        let mut guard = main_api.write().unwrap();
+        let mut guard = main_api.write().await;
         println!("locked");
         println!("start listen");
         guard.start_listen_updates(sender);
@@ -43,7 +44,7 @@ async fn main() {
     let receiver = Mutex::new(receiver);
     spawn(async move {
         loop {
-            let update = receiver.lock().unwrap().recv().unwrap();
+            let update = receiver.lock().await.recv().await;
             println!("{:?}", update);
         }
     });
--- src/tg_client.rs
+++ src/tg_client.rs
@@ -6,7 +6,7 @@ use rtdlib::types::{
     UpdateSupergroupFullInfo,
 };
 use std::io;
-use std::sync::{mpsc, Arc, Condvar, Mutex, RwLock};
+use std::sync::{Arc, Condvar, Mutex};
 use std::thread::JoinHandle;
 use telegram_client::api::aasync::AsyncApi;
 use telegram_client::api::Api;
@@ -17,6 +17,8 @@ use crate::error::Result;
 use futures::{Stream, StreamExt, TryStreamExt};
 use telegram_client::api::aevent::EventApi;
 use telegram_client::errors::TGResult;
+use tokio::runtime;
+use tokio::sync::{mpsc, Mutex as AsyncMutex, RwLock};
 
 #[derive(Clone)]
 pub struct TgClient {
@@ -52,7 +54,7 @@ impl TgClient {
 
     pub fn start_listen_updates(&mut self, channel: mpsc::Sender<TgUpdate>) {
         let listener = self.client.listener();
-        let channel = Arc::new(Mutex::new(channel));
+        let channel = Arc::new(AsyncMutex::new(channel));
         listener.on_update_new_message(get_new_message_handler(channel.clone()));
         listener.on_update_message_content(get_update_content_handler(channel.clone()));
         listener.on_update_chat_photo(get_update_chat_photo_handler(channel.clone()));
@@ -123,7 +125,7 @@ impl TgClient {
             (i64::MAX, client),
             move |(mut from_message_id, client)| async move {
                 let guard = client.clone();
-                let api = guard.read().unwrap();
+                let api = guard.read().await;
                 let history = api.get_chat_history(chat_id, 0, 10, from_message_id).await;
                 let result_messages: Result<Vec<Message>>;
                 // let mut from_message_id = i64::MAX;
@@ -234,65 +236,84 @@ fn get_auth_state_handler(
 }
 
 fn get_new_message_handler(
-    channel: Arc<Mutex<mpsc::Sender<TgUpdate>>>,
+    channel: Arc<AsyncMutex<mpsc::Sender<TgUpdate>>>,
 ) -> impl Fn((&EventApi, &UpdateNewMessage)) -> TGResult<()> + 'static {
+    let rt = runtime::Builder::new_current_thread().build().unwrap();
     move |(_, update)| {
-        let local = channel.lock().unwrap();
-        local.send(TgUpdate::NewMessage(update.clone())).unwrap();
+        rt.block_on(async {
+            let local = channel.lock().await;
+            local.send(TgUpdate::NewMessage(update.clone())).await;
+        });
         Ok(())
     }
 }
 
 fn get_update_content_handler(
-    channel: Arc<Mutex<mpsc::Sender<TgUpdate>>>,
+    channel: Arc<AsyncMutex<mpsc::Sender<TgUpdate>>>,
 ) -> impl Fn((&EventApi, &UpdateMessageContent)) -> TGResult<()> + 'static {
+    let rt = runtime::Builder::new_current_thread().build().unwrap();
     move |(_, update)| {
-        let local = channel.lock().unwrap();
-        local
-            .send(TgUpdate::MessageContent(update.clone()))
-            .unwrap();
+        rt.block_on(async {
+            let local = channel.lock().await;
+            local.send(TgUpdate::MessageContent(update.clone())).await;
+        });
         Ok(())
     }
 }
 
 fn get_update_chat_photo_handler(
-    channel: Arc<Mutex<mpsc::Sender<TgUpdate>>>,
+    channel: Arc<AsyncMutex<mpsc::Sender<TgUpdate>>>,
 ) -> impl Fn((&EventApi, &UpdateChatPhoto)) -> TGResult<()> + 'static {
+    let rt = runtime::Builder::new_current_thread().build().unwrap();
     move |(_, update)| {
-        let local = channel.lock().unwrap();
-        local.send(TgUpdate::ChatPhoto(update.clone())).unwrap();
+        rt.block_on(async {
+            let local = channel.lock().await;
+            local.send(TgUpdate::ChatPhoto(update.clone())).await;
+        });
         Ok(())
     }
 }
 
 fn get_update_chat_title_handler(
-    channel: Arc<Mutex<mpsc::Sender<TgUpdate>>>,
+    channel: Arc<AsyncMutex<mpsc::Sender<TgUpdate>>>,
 ) -> impl Fn((&EventApi, &UpdateChatTitle)) -> TGResult<()> + 'static {
+    let rt = runtime::Builder::new_current_thread().build().unwrap();
     move |(_, update)| {
-        let local = channel.lock().unwrap();
-        local.send(TgUpdate::ChatTitle(update.clone())).unwrap();
+        rt.block_on(async {
+            let local = channel.lock().await;
+            local.send(TgUpdate::ChatTitle(update.clone())).await;
+        });
         Ok(())
     }
 }
 
 fn get_update_supergroup_handler(
-    channel: Arc<Mutex<mpsc::Sender<TgUpdate>>>,
+    channel: Arc<AsyncMutex<mpsc::Sender<TgUpdate>>>,
 ) -> impl Fn((&EventApi, &UpdateSupergroup)) -> TGResult<()> + 'static {
+    let rt = runtime::Builder::new_current_thread().build().unwrap();
     move |(_, update)| {
-        let local = channel.lock().unwrap();
-        local.send(TgUpdate::Supergroup(update.clone())).unwrap();
+        rt.block_on(async {
+            let local = channel.lock().await;
+            local.send(TgUpdate::Supergroup(update.clone())).await;
+        });
         Ok(())
     }
 }
 fn get_update_supergroup_full_info_handler(
-    channel: Arc<Mutex<mpsc::Sender<TgUpdate>>>,
+    channel: Arc<AsyncMutex<mpsc::Sender<TgUpdate>>>,
 ) -> impl Fn((&EventApi, &UpdateSupergroupFullInfo)) -> TGResult<()> + 'static {
+    let rt = runtime::Builder::new_current_thread().build().unwrap();
     move |(_, update)| {
-        let local = channel.lock().unwrap();
-        match local.send(TgUpdate::SupergroupFullInfo(update.clone())) {
-            Err(err) => println!("{:?}", err),
-            _ => {}
-        }
+        rt.block_on(async {
+            let local = channel.lock().await;
+            match local
+                .send(TgUpdate::SupergroupFullInfo(update.clone()))
+                .await
+            {
+                Err(err) => println!("{:?}", err),
+                _ => {}
+            };
+        });
         Ok(())
     }
 }
